69d7a278a34ea17c601eea36ef6e11b945f14daf
Mohammad Hosam <72976485+hosamation@users.noreply.github.com>
2022-04-22 03:18:41 +0200

    Changing Function Context and Built-in Functions

--- app.js
+++ app.js
@@ -1,42 +1,72 @@
-// Regular Function 1
-// let greeting = function() {
-//   return "Hello World";
+// Function Context
+// let greeting = {};
+// greeting.sayHi = function() {
+//   console.log("Hi");
+//   console.log(this);
 // }
 
-// Arrow Function 1
-let greeting = () => "Hello World";
-
-let message = greeting();
-console.log(message); // Hello World
-
-// Regular Function 2
-// let greet = function greetings(name) {
-//   return `Hello ${name}`;
+// function sayHi() {
+//   console.log("Hi");
+//   console.log(this);
 // }
+// let greeting = new sayHi(); // Hi
+                            // {sayHi:f}
 
-// Arrow Function 2
-let greet = name => `Hello ${name}`
-let msg = greet("John");
-console.log(msg); // Hello John
+// greeting.sayHi(); // Hi
 
-let sum = (num1, num2) => num1 + num2;
+// Call 
+let person1 = {name: "John", age:22};
 
-let output = sum(10, 5);
-console.log(output);
-
-/*  this
-    Refers to the owner of the function we are executing  
-    Unlike regular functions,
-    Arrow function do not have their own this value
-*/
-
-let msgThis = {
-  name: "John",
-  regularFunction: function() {
-    console.log(`Hello ${this.name}`);
-  },
-  arrowFunction: () => console.log(`Hi ${this.name}`)
+let sayHi = function(message) {
+  console.log(`${message}, ${this.name}`);
 }
+sayHi.call(person1, "Hi"); // Hi, John
 
-msgThis.regularFunction();
-msgThis.arrowFunction();
+// apply
+function introduction(name, profession) {
+  console.log(`My name is ${name} and I am a ${profession}.`);
+  console.log(this);
+}
+introduction("John", "student");
+introduction.apply(undefined, ["Mary", "Lawyer"]);
+introduction.call(undefined, ["James", "artist"]);
+
+// bind
+let person2 = {
+  name: "Mary",
+  getName: function() {
+    return this.name;
+  }
+};
+let person3 = {name: "John"};
+let getNameCopy = person2.getName.bind(person3);
+console.log(getNameCopy()); // John
+
+// eval
+
+let x =1;
+let y = 2;
+let s = "abc"
+console.log(eval("x + y + 1")); // 4
+console.log(eval("x + y + s")); // 3abc
+
+// parseInt
+console.log(parseInt("F", 16)); // 15
+console.log(parseInt("15", 10)); // 15
+console.log(parseInt("Hi", 10)); // NaN
+
+// parseFloat
+console.log(parseFloat("3.99")); // 3.99
+console.log(parseFloat("3.99e-1")); // 39.9
+console.log(parseFloat("")); // NaN
+
+// escape
+console.log(escape("text")); // text
+console.log(escape(" ")); // %20
+console.log(escape("abc&%")); // abc%26%25
+
+// unescape
+console.log(unescape("text")); // text
+console.log(unescape("%20")); // 
+console.log(unescape("abc%26%25")); // abc&%
+
